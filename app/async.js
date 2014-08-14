2a5e9ab34caad0ab4410e8e49e5a28b1867e877b
Will Hastings <will@objectdotcreate.net>
2014-08-14 13:09:59 -0700

    Finish async behavior exercises.

--- app/async.js
+++ app/async.js
@@ -3,11 +3,21 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define([ 'jquery' ], function($) {
   return {
     async : function(value) {
-
+      var deferred = new $.Deferred();
+      deferred.resolve(value);
+      return deferred.promise();
     },
 
     manipulateRemoteData : function(url) {
-
+      var ajaxPromise = $.ajax({url: url, dataType: 'json'}),
+          deferred = new $.Deferred();
+      ajaxPromise.then(function(data) {
+        var result = data.people.map(function(person) {
+          return person.name;
+        });
+        deferred.resolve(result.sort());
+      });
+      return deferred.promise();
     }
   };
 });
