5b146f8dc3de9e1fd9ea7e61802ea7e48b8a5c42
Will Hastings <will@objectdotcreate.net>
2014-08-14 14:50:56 -0700

    Finish objects and context exercises.

--- app/objects.js
+++ app/objects.js
@@ -3,15 +3,19 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
-
+      return fn.apply(obj, Array.prototype.slice.call(arguments, 1));
     },
 
     alterObjects : function(constructor, greeting) {
-
+      constructor.prototype.greeting = greeting;
     },
 
     iterate : function(obj) {
-
+      var props = [];
+      Object.getOwnPropertyNames(obj).forEach(function(prop) {
+        props.push(prop + ': ' + obj[prop]);
+      });
+      return props;
     }
   };
 });
