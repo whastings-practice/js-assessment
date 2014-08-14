97448baa7d9c27a3100c14b7c175a4a2f1734908
Will Hastings <will@objectdotcreate.net>
2014-08-14 15:33:17 -0700

    Finish regex exercises.

--- app/regex.js
+++ app/regex.js
@@ -3,26 +3,27 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     containsNumber : function(str) {
-
+      return /\d/.test(str);
     },
 
     containsRepeatingLetter : function(str) {
-
+      return /([a-zA-Z]).*\1/.test(str);
     },
 
     endsWithVowel : function(str) {
-
+      return /[aeiou]$/i.test(str);
     },
 
     captureThreeNumbers : function(str) {
-
+      var results = str.match(/(\d{3})/);
+      return results ? results[1] : false;
     },
 
     matchesPattern : function(str) {
-
+      return /^\d{3}-\d{3}-\d{4}$/.test(str);
     },
     isUSD : function(str) {
-
+      return /^\$(?:(?:(?:\d{1,3},)?(?:\d{3},)*\d{3})|(?:\d{1,3}))(?:\.\d{2})?$/.test(str);
     }
   };
 });
