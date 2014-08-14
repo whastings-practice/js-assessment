faaa9678acaa04c07a372683405550c89b3a7762
Will Hastings <will@objectdotcreate.net>
2014-08-14 13:34:31 -0700

    Finish flow control exercises.

--- app/flowControl.js
+++ app/flowControl.js
@@ -11,6 +11,17 @@ define(function() {
       //
       // otherwise the function should return the number, or false if no number
       // was provided or the value provided is not a number
+      if (typeof num !== 'number') { return false; }
+      var byThree = num % 3 === 0,
+          byFive = num % 5 === 0;
+      if (byThree && byFive) {
+        return 'fizzbuzz';
+      } else if (byThree) {
+        return 'fizz';
+      } else if (byFive) {
+        return 'buzz';
+      }
+      return num;
     }
   };
 });
