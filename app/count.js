9dec36e21d2efe5540815817289a6f5e9117a5d4
Will Hastings <will@objectdotcreate.net>
2014-08-14 13:25:06 -0700

    Finish counter exercises.

--- app/count.js
+++ app/count.js
@@ -3,6 +3,24 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function () {
   return {
     count : function (start, end) {
+      var timeoutId,
+          interval = 100,
+          counter = start;
+
+      var increaseCounter = function() {
+        console.log(counter);
+        counter += 1;
+        if (counter <= end) {
+          timeoutId = setTimeout(increaseCounter, interval);
+        }
+      };
+      increaseCounter();
+
+      return {
+        cancel: function() {
+          clearTimeout(timeoutId);
+        }
+      };
     }
   };
 });
