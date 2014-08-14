7e1550b9274b7a6dd0357f3500ccb704485201a4
Will Hastings <will@objectdotcreate.net>
2014-08-14 13:14:07 -0700

    Finish best practices exercises.

--- app/bestPractices.js
+++ app/bestPractices.js
@@ -8,7 +8,7 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     globals : function() {
-      myObject = {
+      var myObject = {
         name : 'Jory'
       };
 
@@ -16,21 +16,22 @@ define(function() {
     },
 
     functions : function(flag) {
+      var getValue;
       if (flag) {
-        function getValue() { return 'a'; }
+        getValue = function getValue() { return 'a'; };
       } else {
-        function getValue() { return 'b'; }
+        getValue = function getValue() { return 'b'; };
       }
 
       return getValue();
     },
 
     parseInt : function(num) {
-      return parseInt(num);
+      return parseInt(num, 10);
     },
 
     identity : function(val1, val2) {
-
+      return val1 === val2;
     }
   };
 });
